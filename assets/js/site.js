d5d16f3292afe2ef9afbb9434944f467f02e7beb
woodcock3 <woodcock3@users.noreply.github.com>
2021-05-19 12:03:33 +0000

    deploy: 9c4f7e004a61f778d12dc6653a5e1b514183b4de

--- assets/js/site.js
+++ assets/js/site.js
@@ -19,10 +19,38 @@ function logoToggle() {
 }
 
 //========================================
+// Logo color when dark background
+//========================================
+var logo-target  = document.querySelector("footer");
+
+var svgColor = document.querySelector(".svgColor")
+var rootElement = document.documentElement
+
+// Next we want to create a function that will be called when that element is intersected
+function callback(entries, observer) {
+  // The callback will return an array of entries, even if you are only observing a single item
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Change logo color
+      svgColor.classList.add("toggle-svg")
+    } else {
+      // Return to original color
+      svgColor.classList.remove("toggle-svg")
+    }
+  });
+}
+    
+// Next we instantiate the observer with the function we created above. This takes an optional configuration
+// object that we will use in the other examples.
+let observer = new IntersectionObserver(callback);
+// Finally start observing the target element
+observer.observe(logo-target);
+
+//========================================
 // Scroll up button
 //========================================
 // We select the element we want to target
-var target = document.querySelector("footer");
+var target  = document.querySelector("footer");
 
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
 var rootElement = document.documentElement
