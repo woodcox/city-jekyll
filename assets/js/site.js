87d736aa7de0313521f25f2ba3e7a3109bd8c0b6
Paul <64870518+woodcock3@users.noreply.github.com>
2021-05-23 17:07:31 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -59,42 +59,6 @@ let observerLogo = new IntersectionObserver(logoColorChange, config);
 // Finally start observing the target element
 observerLogo.observe(logoTarget);
 
-//========================================
-// Scroll up button
-//========================================
-// We select the element we want to target
-var target  = document.querySelector("footer");
-
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
-var rootElement = document.documentElement
-
-// Next we want to create a function that will be called when that element is intersected
-function callback(entries, observer) {
-  // The callback will return an array of entries, even if you are only observing a single item
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // Show button
-      scrollToTopBtn.classList.add("showBtn")
-    } else {
-      // Hide button
-      scrollToTopBtn.classList.remove("showBtn")
-    }
-  });
-}
-
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop);
-    
-// Next we instantiate the observer with the function we created above. This takes an optional configuration object
-let observer = new IntersectionObserver(callback);
-// Finally start observing the target element
-observer.observe(target);
-
 //=====================================================
 // Carousel
 //=====================================================
