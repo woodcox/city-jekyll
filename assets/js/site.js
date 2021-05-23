b640d2625915469b5e976083feae56be1679f79d
woodcock3 <woodcock3@users.noreply.github.com>
2021-05-23 09:18:16 +0000

    deploy: 98c483289a36ea5cc7d7d8fad59b3f4d0148d6eb

--- assets/js/site.js
+++ assets/js/site.js
@@ -210,6 +210,6 @@ var TxtType = function(el, toRotate, period) {
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid {{ site.colors.cityorange }}}";
         document.body.appendChild(css);
     };
