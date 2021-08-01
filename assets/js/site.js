f7521141d5a7cb6f65fae6b121d3e0a94744566d
Paul <64870518+woodcock3@users.noreply.github.com>
2021-08-01 07:53:00 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,12 +5,12 @@
 // Initialize lozad library
 //=======================================
 lozad('.lozad', {
-    load: function(el) {
-        el.src = el.dataset.src;
-        el.onload = function() {
-            el.classList.add('fade')
-        }
-    }
+  load: function(el) {
+    el.src = el.dataset.src;
+      el.onload = function() {
+        el.classList.add('fade')
+      }
+  }
 }).observe()
 
 //=======================================
