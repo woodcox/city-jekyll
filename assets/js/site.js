90d55ddea1fe6191b71bcdf6d925b06712b04423
woodcock3 <woodcock3@users.noreply.github.com>
2021-08-01 06:53:32 +0000

    deploy: f7521141d5a7cb6f65fae6b121d3e0a94744566d

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,12 +2,12 @@
 // Initialize lozad library
 //=======================================
 lozad('.lozad', {
-    load: function(el) {
-        el.src = el.dataset.src;
-        el.onload = function() {
-            el.classList.add('fade')
-        }
-    }
+  load: function(el) {
+    el.src = el.dataset.src;
+      el.onload = function() {
+        el.classList.add('fade')
+      }
+  }
 }).observe()
 
 //=======================================
