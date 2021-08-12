cd2abadadc17846f490a3e724404cafc33483b33
Paul <64870518+woodcock3@users.noreply.github.com>
2021-08-12 04:13:08 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -61,8 +61,8 @@ const swiper = new Swiper('.swiper-container', {
 //======================================
 
 // Create a new slider.
-const slider = new SnapSlider('.slidercontainer', {
-  id: 'example',
+const slider = new SnapSlider('.flex-row-carousel', {
+  id: 'mediaSlider',
   nav: '.slider-nav',
   start: 'first',
 });
