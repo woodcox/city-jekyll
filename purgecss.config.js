c10ab3982f3f17c811c33bccc6d233a84beb6cef
woodcock3 <woodcock3@users.noreply.github.com>
2021-08-26 19:48:47 +0000

    deploy: 3b9563877e6a504ee27cb070bb896777f635857e

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-// purgecss.config.js
-
-module.exports = {
-  // These are the files that Purgecss will search through
-  content: ["./_site/**/*.html"],
-
-  // These are the stylesheets that will be subjected to the purge
-  css: ["./_site/assets/css/main.css"] 
-};
