ebd55fd91016f5122b47f469bebfb1e070b9ba43
Paul <64870518+woodcock3@users.noreply.github.com>
2021-09-01 21:19:41 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
-    require("postcss-scss"), // example of plugin you might use
+    require("nanocss"), // example of plugin you might use
     ...(process.env.JEKYLL_ENV == "production" // example of only using a plugin in production
       ? [require("cssnano")({ preset: "default" })]
       : [])
