d1a8b83e87b87326376588dfb281d295329e8bd8
Paul <64870518+woodcock3@users.noreply.github.com>
2021-09-01 21:15:51 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 // postcss.config.js
 
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: [
-    require("autoprefixer"), // example of plugin you might use
+    require("postcss-scss"), // example of plugin you might use
     ...(process.env.JEKYLL_ENV == "production" // example of only using a plugin in production
       ? [require("cssnano")({ preset: "default" })]
       : [])
