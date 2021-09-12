6489df6f368f72e28167cdf99af3b253c8e74e93
Paul <64870518+woodcock3@users.noreply.github.com>
2021-09-12 09:10:05 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,30 @@
 // postcss.config.js
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./**/*.html', './**/site.js'],
-  fontFace: true,
-  keyframes: true,
-  variables: true
-});
-
-const cssnano = require('cssnano')({ 
-  preset: ['default', {
-    svgo: true,
-    discardComments: {
-      removeAll: true,
-    },
-  }]
-});
-
 if(process.env.NODE_ENV === 'production') {
   module.exports = {
     plugins: [
       require('autoprefixer'),
-      require('cssnano')
-    ]
-  }
-}
+      require('@fullhuman/postcss-purgecss')({
+        content: ['./**/*.html', './**/site.js'],
+        fontFace: true,
+        keyframes: true,
+        variables: true
+      }),
+      require('cssnano')({
+        preset: ['default', {
+          svgo: true,
+          discardComments: {
+            removeAll: true,
+          },
+        }]
+      }),
+    ],
+  },
+};
 
-
-
-module.exports = {
+else(module.exports = {
   plugins: [
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-    ...(process.env.NODE_ENV === 'production' ? [cssnano] : [])
+    require('autoprefixer')
   ],
 };
+
