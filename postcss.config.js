0873da5973747c14df248ccbec7425c57c69d0ed
Paul <64870518+woodcock3@users.noreply.github.com>
2021-09-19 17:27:49 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,18 +3,7 @@
 const autoprefixer = require('autoprefixer');
 const cssnanoConfig = {
     autoprefixer: false,
-    colormin: true,
-    convertValues: true,
-    cssDeclarationSorter: true,
     discardComments: { removeAll: true, },
-    discardDuplicates: true,
-    discardEmpty: true,
-    discardOverridden: true,
-    mergeLonghand: true,
-    mergeRules: true,
-    minifyFontValues: true,
-    minifySelectors: true,
-    uniqueSelectors: true,
     svgo: true,
 };
 const cssnano = require('cssnano')({
