ce0c024cf63d3e8b565737ca55a9161f9a1580dd
Paul <64870518+woodcock3@users.noreply.github.com>
2021-10-16 20:43:21 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -149,36 +149,3 @@ window.onload = function() {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid {{ site.colors.cityorange }}}";
   document.body.appendChild(css);
 };
-
-//=======================================
-// YouTube player API
-//=======================================
-// https://developers.google.com/youtube/iframe_api_reference
-
-// global variable for the player
-var player;
-
-// this function gets called when API is ready to use
-function onYouTubePlayerAPIReady() {
-  // create the global player from the specific iframe (#video)
-  player = new YT.Player("yt-play", {
-    events: {
-      // call this function when player is ready to use
-      onReady: onPlayerReady
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  // bind events
-  var playButton = document.getElementById("yt-play-button");
-  playButton.addEventListener("click", function () {
-    player.playVideo();
-  });
-}
-
-// Inject YouTube API script
-var tag = document.createElement("script");
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
