b9d14c66561f29587627a3dafd47ac3b2d65193a
Paul <64870518+woodcock3@users.noreply.github.com>
2021-11-03 14:05:36 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -11,7 +11,7 @@ const cssnano = require('cssnano')({
   ]
 });
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['index.html', 'assest/js/*.js'],
+  content: ['**/*.html', 'assest/js/*.js'],
   safelist: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track']
 });
 
