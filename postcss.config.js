e61c8fe4560f43804fbba32b5c34e6acb9589200
Paul <64870518+woodcock3@users.noreply.github.com>
2021-11-05 13:37:48 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -10,24 +10,18 @@ const cssnano = require('cssnano')({
   preset: ['default', { cssnanoConfig }
   ]
 });
+
+const uncss = require('postcss-uncss')({
+  html: ['**/*.html']
+});
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['**/*.html', '**/*.js'],
   safelist: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track']
 }
 
-const fs = require('fs');
-
-const options = {
-  html: ['**/*.html']
-};
-
-fs.readFile('css/index.css','utf8', (err, css) => {
-  postcss([require('postcss-uncss')(options)])
-    .process(css, { from: 'css/app.css', to: 'css/filtered.css' })
-
 module.exports = {
   plugins: [
     autoprefixer,
-    ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : [])
+    ...(process.env.NODE_ENV === "production" ? [uncss, cssnano] : [])
   ],
 };
