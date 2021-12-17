307e85688192b49f76df0be615e5f5a911c3047f
Paul <64870518+woodcock3@users.noreply.github.com>
2021-12-17 03:18:09 +0000

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -52,6 +52,43 @@ function hideBTN() {
 }
 
 //=====================================================
+// Copy contact button
+//=====================================================
+
+const aioColors = document.querySelectorAll('.color span');
+
+aioColors.forEach(color => {
+  color.addEventListener('click', () => {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(color);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+      document.execCommand('copy');
+      selection.removeAllRanges();
+
+      const original = color.textContent;
+      color.textContent = 'Copied!';
+      color.classList.add('success');
+
+      setTimeout(() => {
+        color.textContent = original;
+        color.classList.remove('success');
+      }, 1200);
+    } catch(e) {
+      const errorMsg = document.querySelector('.error-msg');
+      errorMsg.classList.add('show');
+
+      setTimeout(() => {
+        errorMsg.classList.remove('show');
+      }, 1200);
+    }
+  });
+});
+
+//=====================================================
 // Smooth scroll (plus polyfill)
 //=====================================================
 const links = document.querySelectorAll("nav ul a");
