93c73e21ec2964e75dc944ac4c5ae728d5a9c83f
woodcock3 <woodcock3@users.noreply.github.com>
2021-12-21 16:53:55 +0000

    deploy: 2d3cf1146456734449001a8491d66d513b55bd09

--- assets/js/site.js
+++ assets/js/site.js
@@ -87,6 +87,13 @@ copyEmailBtn.addEventListener('click', function(event) {
 });
 
 //=====================================================
+// Decryption
+//=====================================================
+const node47 = document.querySelectorAll('decrypted'),
+function r(a,b){return++b?String.fromCharCode((a=a.charCodeAt()+47,a>126?a-94:a)):a.replace(/[^ ]/g,r)};
+document.getElementById( "decrypted" ).innerHTML = r(node47.textContent);
+
+//=====================================================
 // Smooth scroll (plus polyfill)
 //=====================================================
 const links = document.querySelectorAll("nav ul a");
