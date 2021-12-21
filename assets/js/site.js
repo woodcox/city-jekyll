31e8c59d61ffbefcd6c6be5e31328f7d8be8b028
Paul <64870518+woodcock3@users.noreply.github.com>
2021-12-21 04:59:20 +0000

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -89,6 +89,28 @@ aioMailing.forEach(copyemail => {
   });
 });
 
+var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Select the email link anchor text  
+  var emailLink = document.querySelector('.js-emaillink');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+
+  try {  
+    // Now that we've selected the anchor text, execute the copy command  
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+
+  // Remove the selections - NOTE: Should use
+  // removeRange(range) when it is supported  
+  window.getSelection().removeAllRanges();  
+});
+
 //=====================================================
 // Smooth scroll (plus polyfill)
 //=====================================================
