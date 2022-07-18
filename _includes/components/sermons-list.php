4dd3fd3548274beb878bdc88483de4fddda29bdb
Paul <64870518+woodcock3@users.noreply.github.com>
2022-07-18 06:56:10 +0100

    Update sermons-list.php

--- _includes/components/sermons-list.php
+++ _includes/components/sermons-list.php
@@ -1,9 +1,9 @@
 <?php include("/home/ceclee/public_html/php/sermons/include.php"); ?>
 
 <div class="flex-row">
-  <a class="flex-small" aria-label="Listen on Apple Podcasts" target="_blank" href="https://podcasts.apple.com/us/podcast/city-evangelical-church-sermons/id1537249782">{% include svg/apple-podcast.svg %}</a>
-  <a class="flex-small" aria-label="Listen on Amazon Music" target="_blank" href="https://music.amazon.co.uk/podcasts/cfd1efb8-db38-4a35-a90a-cc20787b6fd7/City-Evangelical-Church-Sermons">{% include svg/amazon-podcast.svg %}</a>
-  <a class="flex-small" aria-label="Listen on Google Podcasts" target="_blank" href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9jZWNsZWVkcy5jby51ay9zZXJtb25zLnBocA%3D%3D">{% include svg/google-podcast.svg %}</a>
-  <a class="flex-small" aria-label="Listen on Spotify" target="_blank" href="https://open.spotify.com/show/7mWlXxRZD74wuyfJzAYMFf">{% include svg/spotify-podcast.svg %}</a>
-  <a class="flex-small" aria-label="RSS Feed" target="_blank" href="https://cecleeds.co.uk/sermons.php">{% include svg/rss-feed.svg %}</a>
+  <a class="flex-tiny ty-half" aria-label="Listen on Apple Podcasts" target="_blank" href="https://podcasts.apple.com/us/podcast/city-evangelical-church-sermons/id1537249782">{% include svg/apple-podcast.svg %}</a>
+  <a class="flex-tiny ty-half" aria-label="Listen on Amazon Music" target="_blank" href="https://music.amazon.co.uk/podcasts/cfd1efb8-db38-4a35-a90a-cc20787b6fd7/City-Evangelical-Church-Sermons">{% include svg/amazon-podcast.svg %}</a>
+  <a class="flex-tiny ty-half" aria-label="Listen on Google Podcasts" target="_blank" href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9jZWNsZWVkcy5jby51ay9zZXJtb25zLnBocA%3D%3D">{% include svg/google-podcast.svg %}</a>
+  <a class="flex-tiny ty-half" aria-label="Listen on Spotify" target="_blank" href="https://open.spotify.com/show/7mWlXxRZD74wuyfJzAYMFf">{% include svg/spotify-podcast.svg %}</a>
+  <a class="flex-tiny" aria-label="RSS Feed" target="_blank" href="https://cecleeds.co.uk/sermons.php">{% include svg/rss-feed.svg %}</a>
 </div>
